a76f6fb5e995e0839ec64eeb9526ec55bc3c6180
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-23 12:14:16 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './component/head/Header.js'
+
+const App = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default App
---
+++ src/component/head/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+
+const header = () => {
+  return (
+    <>
+    <header className='header'>
+     <div className='container d-flex'>
+         <div className='logo'>
+             <img src='' alt=''/>
+         </div>
+ 
+         <div className='navlink'>
+             <ul className='link f_flex uppercase'>
+                 <li><a href='#home'>home</a></li>
+                 <li><a href='#features'>features</a></li>
+                 <li><a href='protfolio'>protfolio</a></li>
+                 <li><a href='#resume'>resume</a></li>
+                 <li><a href='#clients'>clients</a></li>
+                 <li><a href='#blog'>blog</a></li>
+                 <li><a href='#contact'>contact</a></li>
+                 <li><button className='home-btn'>BUY NOW</button></li>
+ 
+             </ul>
+ 
+             <button className='toggle'>
+                 <i className='fas fa-times'></i>
+             </button>
+         </div>
+     </div>
+    </header>
+    
+    </>
+  )
+}
+
+export default header
