9bbcdeac45e23e13a1db236d87e3767fd3ddba5b
Monisha Dolai <monishadolai1998@gmail.com>
2022-08-24 19:23:03 +0530

    resume

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from './component/head/Header'
 import Hero from './component/hero/Hero'
-import Protfolio from './component/Portfolio/Protfolio'
 import Resume  from './component/Resume/Resume'
+import Testimonial from './component/Testimonial/Testimonal'
 import './App.css'
 
 const App = () => {
@@ -10,8 +10,8 @@ const App = () => {
     <>
     <Header/>
     <Hero/>
-    <Protfolio/>
     <Resume/>
+    <Testimonial/>
     </>
      )
 }
--- src/component/Resume/ResumeApi.js
+++ src/component/Resume/ResumeApi.js
@@ -7,6 +7,46 @@ const ResumeApi = [
       desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
       rate: "9/10",
     },
+    {
+      id: 2,
+      category: "education",
+      year: "Scholiinng (1997 - 2001)",
+      title: "Personal Portfolio ",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "9/10",
+    },
+    {
+      id: 3,
+      category: "education",
+      year: "Scholiinng (1997 - 2001)",
+      title: "Personal Portfolio ",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "9/10",
+    },
+    {
+      id: 4,
+      category: "experience",
+      year: "Scholiinng (1997 - 2001)",
+      title: "Personal Portfolio ",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "9/10",
+    },
+    {
+      id: 5,
+      category: "experience",
+      year: "Scholiinng (1997 - 2001)",
+      title: "Personal Portfolio ",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "9/10",
+    },
+    {
+      id: 6,
+      category: "experience",
+      year: "Scholiinng (1997 - 2001)",
+      title: "Personal Portfolio ",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "9/10",
+    },
     
   ]
   
